4c36e42135662e6452f33a46f11ca7ffb7e94b1a
Maurice Rahme <mrahme97@gmail.com>
2019-09-27 16:52:16 -0500

    finished ex2, started ex3, still need to plot odom data and perform written ananlysis

--- hw_0/part_a.py
+++ hw_0/part_a.py
@@ -15,11 +15,12 @@ mauricerahme2020@u.northwestern.edu
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
+# Robot class contains position attributes (x, y, theta) and move method
 class Robot():
     def __init__(self, position):
-        # Each node has one parent, but can have multiple children
         self.position = position  # x, y, theta at world origin
 
     def move(self, control):
@@ -31,11 +32,38 @@ class Robot():
         self.position = [x, y, theta]
 
 
+# Exercise 2, plot robot movement using given commands
 def a2():
+    # Initialize Robot instance
     position = [0, 0, 0]
     robot = Robot(position)
     path = []
-    # Exercise 2 in part A of Homework
+    # Manual transcription of controls for Exercise 2
+    control = [
+        position, [0.5, 0, 1], [0, -1 / (2 * np.pi), 1], [0.5, 0, 1],
+        [0, 1 / (2 * np.pi), 1], [0.5, 0, 1]
+    ]
+
+    for t in range(len(control)):
+        # Method in Robot class which updates pose based on controls
+        robot.move(control[t])
+        print(robot.position)
+        path.append(robot.position)
+        plt.title('Motion Iteration: ' + str(t - 1))
+        if t == 0:
+            plt.plot(robot.position[0], robot.position[1], 'ko', markersize=10)
+        else:
+            plt.plot([path[t - 1][0], path[t][0]],
+                     [path[t - 1][1], path[t][1]], '-kd')
+        plt.pause(0.05)
+    return robot
+
+
+# Exercise 3, plot robot movement using Odometry.dat
+def a3(odometry):
+    position = [0, 0, 0]
+    robot = Robot(position)
+    path = []
     control = [
         position, [0.5, 0, 1], [0, -1 / (2 * np.pi), 1], [0.5, 0, 1],
         [0, 1 / (2 * np.pi), 1], [0.5, 0, 1]
@@ -44,14 +72,17 @@ def a2():
     for t in range(len(control)):
 
         robot.move(control[t])
-        path.append(robot.position)
         print(robot.position)
+        path.append(robot.position)
         plt.title('Motion Iteration: ' + str(t - 1))
+        # Plot first position clearly
         if t == 0:
             plt.plot(robot.position[0], robot.position[1], 'ko', markersize=10)
+        # Plot other positions usind lines and diamonds
         else:
             plt.plot([path[t - 1][0], path[t][0]],
                      [path[t - 1][1], path[t][1]], '-kd')
+        # Realtime plot update
         plt.pause(0.05)
     return robot
 
@@ -63,7 +94,28 @@ plt.title('Motion Iteration: 0')
 plt.ylabel('y domain')
 plt.xlabel('x domain')
 
+# Read Odomtry Data using Pandas
+odometry_str = pd.read_table(
+    "ds0/ds0_Odometry.dat",
+    sep="\s+",
+    skiprows=1,
+    usecols=["Time [s]", "forward velocity [m/s]", "angular velocity[rad/s]"],
+    names=["Time [s]", "forward velocity [m/s]", "angular velocity[rad/s]"])
+odometry_str = odometry_str.values.tolist()
+
+# Useful data starts on index 3, preceeded by headings
+# Turn string data into floats
+# 67Hz data recording, dt = 1/67s
+odometry = []
+for i in range(3, len(odometry_str)):
+    odometry.append(np.array(odometry_str[i], dtype=np.float32))
+
+# Exercise 2
 robot = a2()
 
+# Exercise 3
+#robot = a3(odometry)
+
+# Plot final position clearly
 plt.plot(robot.position[0], robot.position[1], 'go', markersize=10)
 plt.show()
