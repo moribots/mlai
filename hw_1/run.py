3fb479344e6adff02f35909223c86d4eb31cb763
Maurice Rahme <mrahme97@gmail.com>
2019-10-19 12:47:29 -0500

    coded a* algo, not tested yet

--- hw_1/run.py
+++ hw_1/run.py
@@ -15,17 +15,10 @@ mauricerahme2020@u.northwestern.edu
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as plticker
 from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 import matplotlib.patches as patches
 from matplotlib.path import Path
 import pandas as pd
-
-
-# Robot class for part a
-class Robot():
-    def __init__(self):
-        self.position = 0
 
 
 # Grid class
@@ -74,6 +67,176 @@ class Grid():
                     for indy in range(len(index_x)):
                         self.centres[index_x[indx], index_y[indy]] = 1000
 
+
+class Node():
+    def __init__(self, position, parent, gcost, hcost, obstacle):
+        self.position = position  # grid coordinates
+        self.parent = parent  # pass another node here
+        self.gcost = gcost
+        self.hcost = hcost
+        self.f = self.gcost + self.hcost
+        self.obstacle = obstacle  # T or F
+
+    # Used in heap queue (priority queue for min)
+    def __lt__(self, neighbour):  # overload operator for heap queue
+        return self.f < neighbour.f  # eval heapq based on .f value
+
+
+class A_star():
+    def __init__(self, grid, start, goal):
+        self.grid = grid
+        self.obstacle_list = self.obstacle_list()
+        self.start = start
+        self.goal = goal
+        self.start_grid = self.world2grid(start)
+        self.goal_grid = self.world2grid(goal)
+
+        self.open_list = []
+        self.closed_list = []
+        self.path = []  # this is in world coord
+        self.start_node = Node(self.start_grid, None, None, None, False)
+        self.goal_node = Node(position=self.goal_grid, None, None, None, False)
+        self.current_node = Node(self.start_grid, None, 0, self.get_dist(self.start_node, self.goal_node), False)  # init at start  -- NEED TO REPLACE 2ND 0 WITH CALC HEUR FCN
+
+    def obstacle_list(self):
+        indxs = np.where(self.grid.centres == 1000)
+        coords = list(zip(indxs[0], indxs[1]))
+
+        return coords
+
+    def world2grid(self, coord):
+        """ Returns grid coordinates of fed world coordinate values
+        """
+        for x in range(len(self.grid.cell_xmin)):
+            if coord[0] >= self.grid.cell_xmin[x] and coord[
+                    0] <= self.grid.cell_xmin[x] + self.grid.cell_size:
+                index_x = x
+        for y in range(len(self.grid.cell_ymin)):
+            if coord[1] >= self.grid.cell_ymin[y] and coord[
+                    1] <= self.grid.cell_ymin[y] + self.grid.cell_size:
+                index_y = y
+
+        return [index_x, index_y]
+
+    def grid2world(self, coord):
+        """ Returns world coordinates of fed grid coordinate values
+        """
+        return [
+            coord[0] + self.grid.cell_size / 2,
+            coord[1] + self.grid.cell_size / 2
+        ]
+
+    def get_dist(self, node1, node2):
+        x_dist = abs(node1.position[0] - node2.position[0])
+        y_dist = abs(node1.position[1] - node2.position[1])
+        if x_dist > y_dist:
+            cost = 14 * y_dist + 10 * (x_dist - y_dist)
+        else:
+            cost = 14 * x_dist + 10 * (y_dist - x_dist)
+        return cost
+
+    def get_neighbours(self, node):
+        neighbours = []
+
+        # Evaluate about 3x3 block
+        for x in range(-1, 2):  # x from -1 to 1
+            for y in range(-1, 2):
+                # skip at x = 0, y = 0
+                if x == 0 and y == 0:
+                    continue
+                else:
+                    check_x = node.position[0] + x
+                    check_y = node.position[1] + y
+
+                    # ensure neighbour within grid bounds
+                    if check_x >= 0 and check_x < self.grid.xmax and check_y >= 0 and check_y < self.grid.ymax:
+                        neighbours.append([check_x, check_y])  # add positions to neighbour lit and compare later
+
+        return neighbours
+
+    def trace_path(self, startnode, currnode):
+        self.goal_node = self.current_node
+
+        while self.current_node.position != self.start_node.position:
+            self.path.append(current_node.position)
+            self.current_node = self.current_node.parent
+
+        self.path.reverse()  # reverse path 
+
+        for i in range(len(self.path)):
+            self.path[i] = self.grid2world(self.path[i])
+
+
+
+
+    def plan(self):
+        self.open_list.append(self.current_node)
+
+        # REPLACE WITH HEAPQ FOR FASTER LOOP
+        while len(self.open_list) > 0:
+
+            self.current_node = self.open_list[0]
+
+            for i in range(1, len(self.open_list)):  # start at 1 since 0 is current
+                if self.open_list[i].f < self.current_node.f or (if self.open_list[i].f == self.current_node.f and self.open_list[i].hcost < self.current_node.hcost):
+                    self.current_node = self.open_list[i]
+
+            self.open_list.pop(self.current_node)
+            self.closed_list.append(self.current_node)
+
+            if self.current_node.position == self.goal_node.position:
+                self.trace_path(self.start_node, self.current_node)
+                break
+
+            for neighbour in self.get_neighbours(self.current_node):
+                skip = False
+                closed = False
+                opened = False
+                obstacle_node = False
+
+                # see if matches coords in closed list
+                for node in self.closed_list:
+                    if neighbour[0] == node.position[0] and neighbour[1] == node.position[1]:
+                        # node exists in closed lit
+                        closed = True
+                        skip = True
+
+                # see if index matches obstacle list
+                for obstacle in self.obstacle_list:
+                    if neighbour[0] == obstacle.position[0] and neighbour[1] == obstacle.position[1]:
+                        obstacle_node = True
+                        skip = True
+
+                # see if matches coords in open list
+                for node in self.open_list:
+                    if neighbour[0] == node.position[0] and neighbour[1] == node.position[1]:
+                        # node exists in open list
+                        neighbour_node = node
+                        opened = True
+
+                # if in none of these lists, create new node
+                if closed is False and opened is False and obstacle_node is False and skip is False:
+                    neighbour_temp = Node(neighbour, None, None, None, False)
+                    h_cost = self.get_dist(neighbour_temp, self.goal_node)
+                    g_cost = self.current_node.gcost + self.get_dist(neighbour_temp, self.current_node)
+                    neighbour_node = Node(neighbour, self.current_node, g_cost, h_cost, False)
+                    self.open_list.append(neighbour_node)
+
+                elif skip = True:
+                    continue
+                elif open is True:
+                    # h_cost = self.get_dist(neighbour_node, self.goal_node)
+                    g_cost = self.current_node.gcost + self.get_dist(neighbour_node, self.current_node)
+
+                    if g_cost < neighbour_node.gcost or self.open_list:
+                        neighbour_node.gcost = g_cost
+                        neighbour_node.parent = self.current_node
+
+"""
+class Naive_star():
+    def __init__(self, a_grid):
+        self.x = 0
+"""
 
 # Read .dat Files using Pandas
 def read_dat(start_index, file_path, usecols):
