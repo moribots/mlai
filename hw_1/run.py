bac04c37d6f55e417a9d28efcd4b6322df3997ef
Maurice Rahme <mrahme97@gmail.com>
2019-10-21 15:27:39 -0500

    made online version
    slightly buggy on fine grid for set C

--- hw_1/run.py
+++ hw_1/run.py
@@ -96,7 +96,7 @@ class Node():
         # eval heapq based on this value
         # of f + h, which encompasses cases where
         # f = f and we must choose using h value
-        return self.f < neighbour.f
+        return self.heap < neighbour.heap
 
 
 class A_star():
@@ -149,13 +149,13 @@ class A_star():
     def get_dist(self, node1, node2):
         x_dist = abs(node1.position[0] - node2.position[0])
         y_dist = abs(node1.position[1] - node2.position[1])
-        """
+
         if x_dist > y_dist:
             cost = 1.4 * y_dist + 1.0 * (x_dist - y_dist)
         else:
             cost = 1.4 * x_dist + 1.0 * (y_dist - x_dist)
-        """
-        cost = np.sqrt(x_dist**2 + y_dist**2)
+
+        # cost = np.sqrt(x_dist**2 + y_dist**2)
         return cost
 
     def get_neighbours(self, node):
@@ -309,11 +309,220 @@ class A_star():
                     # pprint(vars(neighbour_node))
 
 
-"""
-class Naive_star():
-    def __init__(self, a_grid):
-        self.x = 0
-"""
+class A_star_online():
+    def __init__(self, grid, start, goal):
+        self.grid = grid
+        self.obstacle_list = self.obstacle_list()
+        self.start = start
+        self.goal = goal
+        self.start_grid = self.world2grid(start)
+        self.goal_grid = self.world2grid(goal)
+
+        self.open_list = []
+        self.closed_list = []
+        self.path = []  # this is in world coord
+        self.start_node = Node(self.start_grid, None, 0, 0, False)
+        self.goal_node = Node(self.goal_grid, None, 0, 0, False)
+        self.current_node = Node(
+            self.start_grid, None, 0,
+            self.get_dist(self.start_node, self.goal_node), False
+        )  # init at start  -- NEED TO REPLACE 2ND 0 WITH CALC HEUR FCN
+        self.neighbour_list = []  # used in online v to select best n
+
+    def obstacle_list(self):
+        indxs = np.where(self.grid.centres == 1000)
+        coords = list(zip(indxs[0], indxs[1]))
+
+        return coords
+
+    def world2grid(self, coord):
+        """ Returns grid coordinates of fed world coordinate values
+        """
+        for x in range(len(self.grid.cell_xmin)):
+            if coord[0] >= self.grid.cell_xmin[x] and coord[
+                    0] <= self.grid.cell_xmin[x] + self.grid.cell_size:
+                index_x = x
+        for y in range(len(self.grid.cell_ymin)):
+            if coord[1] >= self.grid.cell_ymin[y] and coord[
+                    1] <= self.grid.cell_ymin[y] + self.grid.cell_size:
+                index_y = y
+
+        return [index_x, index_y]
+
+    def grid2world(self, coord):
+        """ Returns world coordinates of fed grid coordinate values
+        """
+        c = self.grid.cell_size
+        x = c * (coord[0] + (c / 2)) + self.grid.xmin
+        y = c * (coord[1] + (c / 2)) + self.grid.ymin
+        return [x, y]
+
+    def get_dist(self, node1, node2):
+        x_dist = abs(node1.position[0] - node2.position[0])
+        y_dist = abs(node1.position[1] - node2.position[1])
+        if x_dist > y_dist:
+            cost = 1.4 * y_dist + 1.0 * (x_dist - y_dist)
+        else:
+            cost = 1.4 * x_dist + 1.0 * (y_dist - x_dist)
+        #cost = np.sqrt(x_dist**2 + y_dist**2)
+        #cost = (x_dist**2 + y_dist**2)
+        return cost
+
+    def get_neighbours(self, node):
+        neighbours = []
+        # print("trying to get neighbours")
+
+        # Evaluate about 3x3 block
+        for x in range(-1, 2):  # x from -1 to 1
+            for y in range(-1, 2):
+                # skip at x = 0, y = 0
+                if x == 0 and y == 0:
+                    continue
+                else:
+                    check_x = node.position[0] + x
+                    check_y = node.position[1] + y
+                    # print("Checks: {}".format([check_x, check_y]))
+
+                    # ensure neighbour within grid bounds
+                    if check_x >= 0 and check_x < (
+                            self.grid.xmax / self.grid.cell_size -
+                            self.grid.xmin / self.grid.cell_size
+                    ) and check_y >= 0 and check_y < (
+                            self.grid.ymax / self.grid.cell_size -
+                            self.grid.ymin / self.grid.cell_size):
+                        neighbours.append([
+                            check_x, check_y
+                        ])  # add positions to neighbour lit and compare later
+
+        return neighbours
+
+    def trace_path(self, startnode, currnode):
+        self.goal_node = self.current_node
+
+        while self.current_node.position != self.start_node.position:
+            self.path.append(self.current_node.position)
+            self.current_node = self.current_node.parent
+
+        self.path.reverse()  # reverse path
+
+        # bring back to world coord
+        for i in range(len(self.path)):
+            self.path[i] = self.grid2world(self.path[i])
+
+        path = self.path
+        path.insert(0, self.start)
+
+        return path
+
+    def plan(self):
+        self.open_list.append(self.current_node)
+
+        heapq.heapify(self.open_list)
+
+        it = 0
+        while len(self.open_list) > 0:
+            # print(it)
+            # print("\n")
+            # print("Open List Size: {}".format(len(self.open_list)))
+            # print("The current node is:")
+            # pprint(vars(self.current_node))
+            it += 1
+
+            # self.current_node = self.open_list[0]
+            """
+            # REPLACED WITH HEAPQ FOR FASTER LOOP
+            for i in range(1, len(
+                    self.open_list)):  # start at 1 since 0 is current
+                # print(" node i cost: {}".format(self.open_list[i].hcost))
+                if self.open_list[i].f < self.current_node.f:
+                    self.current_node = self.open_list[i]
+                elif self.open_list[
+                        i].f == self.current_node.f and self.open_list[
+                            i].hcost < self.current_node.hcost:
+                    self.current_node = self.open_list[i]
+            # print("The new current node is:")
+            # pprint(vars(self.current_node))
+
+            index_to_pop = self.open_list.index(self.current_node)
+            self.open_list.pop(index_to_pop)
+            self.closed_list.append(self.current_node)
+            """
+            # Simultaneously set current node and remove it from openlist
+            self.current_node = heapq.heappop(self.open_list)
+            # pprint(vars(self.current_node))
+            # Heapq unnecessary for closed list
+            self.closed_list.append(self.current_node)
+
+            if self.current_node.position == self.goal_node.position:
+                return self.trace_path(self.start_node, self.current_node)
+                print("goal found after {} iterations!".format(it))
+                break
+
+            self.neighbour_list = []  # restart neighbour list every iteration
+            heapq.heapify(self.neighbour_list)
+
+            for neighbour in self.get_neighbours(self.current_node):
+                # print("Neighbour pos: {}".format(neighbour))
+                skip = False
+                closed = False
+                opened = False
+                obstacle_node = False
+
+                # see if matches coords in closed list
+                for node in self.closed_list:
+                    if neighbour[0] == node.position[0] and neighbour[
+                            1] == node.position[1]:
+                        # node exists in closed lit
+                        closed = True
+                        skip = True
+
+                # see if index matches obstacle list
+                for obstacle in self.obstacle_list:
+                    if neighbour[0] == obstacle[0] and neighbour[
+                            1] == obstacle[1]:
+                        obstacle_node = True
+                        skip = True
+
+                # see if matches coords in open list
+                for node in self.open_list:
+                    if neighbour[0] == node.position[0] and neighbour[
+                            1] == node.position[1]:
+                        # node exists in open list
+                        neighbour_node = node
+                        opened = True
+
+                # if in none of these lists, create new node
+                if closed is False and opened is False and obstacle_node is False:
+                    neighbour_temp = Node(neighbour, None, 0, 0, False)
+                    h_cost = self.get_dist(neighbour_temp, self.goal_node)
+                    g_cost = self.current_node.gcost + self.get_dist(
+                        neighbour_temp, self.current_node)
+                    neighbour_node = Node(neighbour, self.current_node, g_cost,
+                                          h_cost, False)
+
+                    # Push to the right index by comparing .heap
+                    # attribute defined in node class under __lt__
+                    # (less than)
+                    heapq.heappush(self.neighbour_list, neighbour_node)
+                    # self.open_list.append(neighbour_node)
+                    # print("The chosen node is:")
+                    # pprint(vars(neighbour_node))
+
+                elif skip is True:
+                    continue
+                elif open is True:
+                    # h_cost = self.get_dist(neighbour_node, self.goal_node)
+                    g_cost = self.current_node.gcost + self.get_dist(
+                        neighbour_node, self.current_node)
+
+                    if g_cost < neighbour_node.gcost or self.open_list:
+                        neighbour_node.gcost = g_cost
+                        neighbour_node.parent = self.current_node
+                    # print("The chosen node is:")
+                    # pprint(vars(neighbour_node))
+            if len(self.neighbour_list) > 0:
+                neighbour_node = heapq.heappop(self.neighbour_list)
+                heapq.heappush(self.open_list, neighbour_node)
 
 
 # Read .dat Files using Pandas
@@ -414,8 +623,8 @@ def plot(landmark_list, a_grid, path, neighbours, exp_nodes):
 
     # Turn grid on for both major and minor ticks and style minor slightly
     # differently.
-    ax.grid(which='major', color='darkviolet', linestyle='--')
-    ax.grid(which='minor', color='darkviolet', linestyle='--')
+    ax.grid(which='major', color='darkviolet', linestyle='-')
+    ax.grid(which='minor', color='darkviolet', linestyle='-')
 
     plt.title('A* Search')
     plt.axis([-2, 5, -6, 6])
@@ -437,10 +646,6 @@ def plot(landmark_list, a_grid, path, neighbours, exp_nodes):
 
 
 def a3(landmark_list, start, goal):
-    return True
-
-
-def a5(landmark_list, start, goal):
     grid_size = 1
     a_grid = Grid(grid_size, landmark_list)
     astar = A_star(a_grid, start, goal)
@@ -462,10 +667,32 @@ def a5(landmark_list, start, goal):
     plot(landmark_list, a_grid, path, open_l, closed_l)
 
 
+def a5(landmark_list, start, goal):
+    grid_size = 1
+    a_grid = Grid(grid_size, landmark_list)
+    astar = A_star_online(a_grid, start, goal)
+
+    path = astar.plan()
+
+    open_list = astar.open_list
+    open_l = []
+    for node in open_list:
+        position = astar.grid2world(node.position)
+        open_l.append(position)
+
+    closed_list = astar.closed_list
+    closed_l = []
+    for node in closed_list:
+        position = astar.grid2world(node.position)
+        closed_l.append(position)
+
+    plot(landmark_list, a_grid, path, open_l, closed_l)
+
+
 def a7(landmark_list, start, goal):
     grid_size = 0.1
     a_grid = Grid(grid_size, landmark_list)
-    astar = A_star(a_grid, start, goal)
+    astar = A_star_online(a_grid, start, goal)
 
     path = astar.plan()
 
