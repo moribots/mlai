62498c1d175b2edda6d6e62638cd0a0d65fcdc02
Maurice Rahme <mrahme97@gmail.com>
2019-11-08 00:01:54 -0600

    LWLR getting singular matrix error...

--- hw_2/sine_test.py
+++ hw_2/sine_test.py
@@ -33,21 +33,59 @@ def sine(cycles, points):
     return x, y
 
 
-def plot(x, y):
+def lwlr_pt(testPoint, xArr, yArr, k):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    # Create diagonal matrix
+    m = np.shape(xMat)[0]
+    weights = np.mat(np.eye((m)))
+    # Populate weights with exponentially decaying values
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = np.exp(diffMat * diffMat.T / (-2.0 * k**2))
+
+    # find extimate for testpoint
+    xTwx = xMat.T * (weights * xMat)
+    # if np.linalg.det(xTwx) == 0.0:
+    #     print("This matrix is singular, cannot do inverse")
+    #     return
+
+    ws = xTwx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+def lwlr(testArr, xArr, yArr, k):
+    m = np.shape(testArr)[0]
+    yHat = np.zeros(m)
+    # Replace each entry by its weight
+    for i in range(m):
+        yHat[i] = lwlr_pt(testArr[i], xArr, yArr, k)
+    return yHat
+
+
+def plot(x, y, yhat):
     # Sine Plot
     plt.autoscale(enable=True, axis='both', tight=None)
     plt.title('Noisy Sine Wave')
     plt.ylabel('y')
     plt.xlabel('x')
     plt.scatter(x, y)
+    plt.scatter(x, yhat, color='r')
 
     plt.show()
 
 
-
 def main():
     x, y = sine(2, 500)
-    plot(x, y)
+    # convert into arrays
+    x = np.array(x)
+    y = np.array(y)
+    y = y.flatten()
+
+    # perform LWLR
+    k = 0.1
+    yhat = lwlr(x[:250], x[251:], y[251:], k)
+    plot(x, y, yhat)
 
 
 if __name__ == "__main__":
