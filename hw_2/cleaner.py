f1fb9f9f2d7c7a78a370ddb3a72c84c78e68dd43
Maurice Rahme <mrahme97@gmail.com>
2019-11-11 11:04:58 -0600

    cleaned up data plots

--- hw_2/cleaner.py
+++ hw_2/cleaner.py
@@ -81,43 +81,64 @@ def get_gtr(odometry, ground_truth):
 
 
 def fwd_prop(ground_truth, odometry, dt):
-        """ Forward propagate the motion model
+    """ Forward propagate the motion model
             using dead reckoning
+
+            1 of 2 assumptions:
+            1- odom applied between t_odom and t_gt
+            starting at 0 vel
+            2- odom applied between t-1_gt and t_gt
+            starting at 0 vel
         """
-        x = ground_truth[1] + odometry[1] * np.cos(
-            ground_truth[3]) * dt
-        y = ground_truth[2] + odometry[1] * np.sin(
-            ground_truth[3]) * dt
-        theta = ground_truth[3] + odometry[2] * dt
-        # return new ground truth at time stamp
-        return [dt + odometry[0], x, y, theta]
+    x = ground_truth[1] + odometry[1] * np.cos(ground_truth[3]) * dt
+    y = ground_truth[2] + odometry[1] * np.sin(ground_truth[3]) * dt
+    theta = ground_truth[3] + odometry[2] * dt
+    # return new ground truth at time stamp
+    return [dt + odometry[0], x, y, theta]
 
 
-def viz_data(fwd_gt, gt):
+def viz_data(fwd_gt, gt, odom):
     """ Plot error in data
+
+
+        NOTE:
+
+        input [v w]^T * dt
+        output [dx dy dtheta] (all ground truth)
+
+        also assuming no prior controls and that controls extend to next gt
     """
     # start by turning data into 2D array
     fwd_gt = np.array(fwd_gt)
     gt = np.array(gt)
+    odom = np.array(odom)
+
+    n = np.shape(odom)[0]
+
+    for i in range(n - 1):
+        odom[i, 1:] = odom[i, 1:] * (gt[i + 1, 0] - gt[i, 0])
 
     # # now calculate distance magnitude for each set
     # dmag_fgt = np.sqrt(np.square(fwd_gt[:,1]) + np.square(fwd_gt[:,2]))
-    dmag_gt = np.sqrt(np.square(gt[:, 1]) + np.square(gt[:, 2]))
+    dmag_x = gt[:, 1]
+    dmag_y = gt[:, 2]
+    dmag_h = gt[:, 3]
+    dmag_gt = np.sqrt(np.square(dmag_x) + np.square(dmag_y))
 
     # now calculate x difference
-    x_diff = fwd_gt[:, 1] - gt[:, 1]
-    y_diff = fwd_gt[:, 2] - gt[:, 2]
+    x_diff = fwd_gt[:, 1] - dmag_x
+    y_diff = fwd_gt[:, 2] - dmag_y
 
     # now calculate distance magnitude difference
     diff_dmag = np.sqrt(np.square(x_diff) + np.square(y_diff))
 
     # now calculate heading difference for each set
-    diff_head = fwd_gt[:, 3] - gt[:, 3]
+    diff_head = fwd_gt[:, 3] - dmag_h
 
-    return diff_dmag, diff_head, dmag_gt
+    return diff_dmag, diff_head, dmag_gt, dmag_x, dmag_y, dmag_h, odom
 
 
-def plot(odom, diff_dmag, diff_head, dmag_gt):
+def plot(odom, diff_dmag, diff_head, dmag_gt, dmag_x, dmag_y, dmag_h, odom_dt):
     """ Plot error in delta_gt vs delta_gt_dreck
     """
 
@@ -128,6 +149,9 @@ def plot(odom, diff_dmag, diff_head, dmag_gt):
     diff_dmag = np.delete(diff_dmag, (0), axis=0)
     diff_head = np.delete(diff_head, (0), axis=0)
     dmag_gt = np.delete(dmag_gt, (0), axis=0)
+    dmag_x = np.delete(dmag_x, (0), axis=0)
+    dmag_y = np.delete(dmag_y, (0), axis=0)
+    dmag_h = np.delete(dmag_h, (0), axis=0)
 
     # Initialize Plot dmagv
     plt.figure(1)
@@ -135,39 +159,87 @@ def plot(odom, diff_dmag, diff_head, dmag_gt):
     plt.title('Distance error for v commands')
     plt.ylabel('dmag [m]')
     plt.xlabel('v [m/s]')
-    plt.scatter(odom[:, 1], diff_dmag)
+    plt.scatter(odom_dt[:, 1], diff_dmag)
 
     # Initialize Plot dmagw
     plt.figure(2)
     plt.autoscale(enable=True, axis='both', tight=None)
     plt.title('Distance error for w commands')
-    plt.ylabel('hdiff [rad]')
-    plt.xlabel('w [rad/s]')
-    plt.scatter(odom[:, 2], diff_dmag)
-
-    # Initialize Plot hmagv
-    plt.figure(3)
-    plt.autoscale(enable=True, axis='both', tight=None)
-    plt.title('Heading error for v commands')
     plt.ylabel('dmag [m]')
-    plt.xlabel('v [m/s]')
-    plt.scatter(odom[:, 1], diff_head)
-
-    # Initialize Plot hmagw
-    plt.figure(4)
-    plt.autoscale(enable=True, axis='both', tight=None)
-    plt.title('Heading error for w commands')
-    plt.ylabel('hdiff [rad]')
     plt.xlabel('w [rad/s]')
-    plt.scatter(odom[:, 2], diff_head)
+    plt.scatter(odom_dt[:, 2], diff_dmag)
 
-    # Initialize Plot dmag_gt
-    plt.figure(5)
+    # # Initialize Plot hmagv
+    # plt.figure(3)
+    # plt.autoscale(enable=True, axis='both', tight=None)
+    # plt.title('Heading error for v commands')
+    # plt.ylabel('hdiff [rad]')
+    # plt.xlabel('v [m/s]')
+    # plt.scatter(odom[:, 1], diff_head)
+
+    # # Initialize Plot hmagw
+    # plt.figure(4)
+    # plt.autoscale(enable=True, axis='both', tight=None)
+    # plt.title('Heading error for w commands')
+    # plt.ylabel('hdiff [rad]')
+    # plt.xlabel('w [rad/s]')
+    # plt.scatter(odom[:, 2], diff_head)
+
+    # # Initialize Plot dmag_gt
+    # plt.figure(5)
+    # plt.autoscale(enable=True, axis='both', tight=None)
+    # plt.title('fig')
+    # plt.ylabel('[m]')
+    # plt.xlabel('v [m/s]')
+    # plt.scatter(odom[:, 1], dmag_gt)
+
+    # Initialize Plot dmag_x
+    plt.figure(6)
     plt.autoscale(enable=True, axis='both', tight=None)
-    plt.title('fig')
+    plt.title('dmagx')
     plt.ylabel('[m]')
     plt.xlabel('v [m/s]')
-    plt.scatter(odom[:, 1], dmag_gt)
+    plt.scatter(odom_dt[:, 1], dmag_x)
+
+    # Initialize Plot dmag_x
+    plt.figure(7)
+    plt.autoscale(enable=True, axis='both', tight=None)
+    plt.title('dmagx')
+    plt.ylabel('[m]')
+    plt.xlabel('w [rad/s]')
+    plt.scatter(odom_dt[:, 2], dmag_x)
+
+    # Initialize Plot dmag_y
+    plt.figure(8)
+    plt.autoscale(enable=True, axis='both', tight=None)
+    plt.title('dmagy')
+    plt.ylabel('[m]')
+    plt.xlabel('v [m/s]')
+    plt.scatter(odom_dt[:, 1], dmag_y)
+
+    # Initialize Plot dmag_y
+    plt.figure(9)
+    plt.autoscale(enable=True, axis='both', tight=None)
+    plt.title('dmagy')
+    plt.ylabel('[m]')
+    plt.xlabel('w [rad/s]')
+    plt.scatter(odom_dt[:, 2], dmag_y)
+
+    # Initialize Plot dmag_h
+    plt.figure(10)
+    plt.autoscale(enable=True, axis='both', tight=None)
+    plt.title('dmagh')
+    plt.ylabel('[rad]')
+    plt.xlabel('v [m/s]')
+    plt.scatter(odom_dt[:, 1], dmag_h)
+
+    # Initialize Plot dmag_h
+    plt.figure(11)
+    plt.autoscale(enable=True, axis='both', tight=None)
+    plt.title('dmagh')
+    plt.ylabel('[rad]')
+    plt.xlabel('w [rad/s]')
+    plt.scatter(odom_dt[:, 2], dmag_h)
 
     plt.show()
 
@@ -207,10 +279,8 @@ def main():
     fwd_gt = get_gtr(odom, gt)
 
     # compute error in dead reckoning set
-    diff_dmag, diff_head, dmag_gt = viz_data(fwd_gt, gt)
-
-    # plot errors
-    plot(odom, diff_dmag, diff_head, dmag_gt)
+    diff_dmag, diff_head, dmag_gt, dmag_x, dmag_y, dmag_h, odom_dt = viz_data(
+        fwd_gt, gt, odom)
 
     # Create CSV File
     with open("odom_train.csv", "w+") as my_csv:
@@ -222,6 +292,12 @@ def main():
     with open("gt_deadreck.csv", "w+") as my_csv:
         csvWriter = csv.writer(my_csv, delimiter=',')
         csvWriter.writerows(fwd_gt)
+    with open("odom_dt.csv", "w+") as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=',')
+        csvWriter.writerows(odom_dt)
+
+    # plot errors
+    plot(odom, diff_dmag, diff_head, dmag_gt, dmag_x, dmag_y, dmag_h, odom_dt)
 
 
 if __name__ == "__main__":
